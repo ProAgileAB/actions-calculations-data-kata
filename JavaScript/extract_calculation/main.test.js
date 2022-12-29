11ed476e3f2ee206705cdd99acf2e77d0f957d9a
Olof Bjarnason <olof.bjarnason@gmail.com>
2022-12-29 11:38:09 +0100

    Switch to vitest

---
+++ JavaScript/extract_calculation/main.test.js
@@ -0,0 +1,7 @@
+import {describe, it} from 'vitest'
+import {calc_cart_total} from './main'
+
+describe("shopping cart total price calculation", () => {
+    it("gives the price item if 1 item with amount 1 is in cart", () => {
+    })
+})
