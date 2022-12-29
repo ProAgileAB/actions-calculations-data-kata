6c88e48800975eb81dcfbbca99f2ae1319a180d6
Olof Bjarnason <olof.bjarnason@gmail.com>
2022-12-29 12:06:25 +0100

    Exercise 1

--- JavaScript/extract_calculation/main.js
+++ JavaScript/extract_calculation/main.js
@@ -1,15 +1,15 @@
 // Part of an imaginary web shop application using jQuery (yes!) to manipulate the DOM
 
 // Global variables
-let shopping_cart = [];
-let shopping_cart_total = 0;
+let shopping_cart = []; // action (global variable)
+let shopping_cart_total = 0; // action (global variable)
 
 /*
 This function is called 'here and there' in the code base, when re-calculating the
 Cart Total Price is deemed necessary, e.g. when adding an item to the cart, or
 clicking + icon on an already existing item.
  */
-function calc_cart_total() {
+function calc_cart_total() { // action (contains actions)
     shopping_cart_total = 0;
     for (var i = 0; i < shopping_cart.length; i++) {
         var item = shopping_cart[i];
@@ -21,15 +21,15 @@ function calc_cart_total() {
 }
 
 // Updates the Cart Total Price DOM element
-function set_cart_total_dom() {
+function set_cart_total_dom() { // action
     // findCartTotalDOMElement omitted from exercise code
     findCartTotalDOMElement().innerText = '' + shopping_cart_total;
 }
 
-function update_shipping_icons() {
+function update_shipping_icons() { // action
     // Omitted from exercise; updates DOM elements (img's) to tell customer if there is free shipping or not
 }
 
-function update_tax_dom() {
+function update_tax_dom() { // action
     // Omitted from exercise; updates DOM element for tax total (moms/VAT)
 }
