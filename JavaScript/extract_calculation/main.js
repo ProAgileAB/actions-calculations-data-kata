3d447f4353c40c49aaa864cffcc2a3826fdae4b8
Olof Bjarnason <olof.bjarnason@gmail.com>
2022-12-29 11:42:02 +0100

    Formatting

--- JavaScript/extract_calculation/main.js
+++ JavaScript/extract_calculation/main.js
@@ -9,7 +9,7 @@ let shopping_cart_total = 0;
 // clicking + icon on an already existing item.
 function calc_cart_total() {
     shopping_cart_total = 0;
-    for(var i = 0; i < shopping_cart.length; i++) {
+    for (var i = 0; i < shopping_cart.length; i++) {
         var item = shopping_cart[i];
         shopping_cart_total += item.price;
     }
